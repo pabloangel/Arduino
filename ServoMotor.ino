adeb0f0e903137e787671fa61aae1967bed2f45a
pabloangel <32901188+pabloangel@users.noreply.github.com>
2023-01-11 11:47:46 -0300

    Add files via upload
    Codigo relacionado al uso de servo motores en arduino y su uso en grados

---
+++ ServoMotor.ino
@@ -0,0 +1,60 @@
+#include <Servo.h>
+Servo servomotor;
+
+void setup() {
+  servomotor.attach(2);
+
+}
+
+void loop() {
+  servomotor.write(0);
+  delay(1000);
+  servomotor.write(10);
+  delay(1000);
+  servomotor.write(20);
+  delay(1000);
+  servomotor.write(30);
+  delay(1000);
+  servomotor.write(40);
+  delay(1000);
+  servomotor.write(50);
+  delay(1000);
+  servomotor.write(60);
+  delay(1000);
+  servomotor.write(70);
+  delay(1000);
+  servomotor.write(80);
+  delay(1000);
+  servomotor.write(90);
+  delay(1000);
+  servomotor.write(100);
+  delay(1000);
+  servomotor.write(110);
+  delay(1000);
+  servomotor.write(120);
+  delay(1000);
+  servomotor.write(130);
+  delay(1000);
+  servomotor.write(140);
+  delay(1000);
+  servomotor.write(150);
+  delay(1000);
+  servomotor.write(160);
+  delay(1000);
+  servomotor.write(170);
+  delay(1000);
+  servomotor.write(180);
+  delay(1000);
+  servomotor.write(190);
+  delay(1000);
+  servomotor.write(200);
+  delay(1000);
+  servomotor.write(210);
+  delay(1000);
+  servomotor.write(220);
+  delay(1000);
+  servomotor.write(230);
+  delay(1000);
+  servomotor.write(240);
+  delay(1000);
+}
